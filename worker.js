8323891ce106c00e555eea1b797660d050851081
文化資料登録室 <08084947685jddhdj@gmail.com>
2026-09-14 02:14:38 +0900

    Point Worker entry to production numbering verifier wrapper

--- worker.js
+++ worker.js
@@ -1,3 +1,3 @@
-import app from "./worker-dashboard10.js";
-export { RegistrationIssuer } from "./worker-dashboard10.js";
+import app from "./worker-dashboard11.js";
+export { RegistrationIssuer } from "./worker-dashboard11.js";
 export default app;
